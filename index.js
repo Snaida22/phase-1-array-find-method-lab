77e7ac9d89c38178e4b0720e23a43899550d4a77
Snaida.Kabalika <snaida.kabalika@student.moringaschool.com>
2022-06-11 19:52:36 +0300

    commit tree babc351ce51a8c5bde1cb3a02e0192dda80272e7

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+const superbowlWin = (record) => {
+    let myResult = record.find((item) => item.result === "W");
+    if (myResult) {
+        return myResult.year;
+    } else {
+        return undefined;
+    }
+}
